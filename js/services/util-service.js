70bade48b898e2bead5bf64d6ee7f26d54c89268
Lihi Ben Shimol <lihibs26@gmail.com>
2022-12-14 13:42:30 +0200

    my first commit

---
+++ js/services/util-service.js
@@ -0,0 +1,45 @@
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.offsetWidth - 20
+}
+
+function onClearCanvas() {
+    // Sets all pixels in the rectangle defined by starting point (x, y) and size (width, height)
+    // to transparent black, erasing any previously drawn content.
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
+    // You may clear part of the canvas
+    // gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height/4)
+}
+
+function onDownloadCanvas(elLink) {
+    // Gets the canvas content and convert it to base64 data URL that can be save as an image
+    const data = gElCanvas.toDataURL() // Method returns a data URL containing a representation of the image in the format specified by the type parameter.
+    elLink.href = data // Put it on the link
+    // elLink.download = 'shuki' // Can change the name of the file
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+    // Pack the image for delivery
+    const formData = new FormData()
+    formData.append('img', imgDataUrl)
+    console.log('formData:', formData)
+    // Send a post req with the image to the server
+    fetch('//ca-upload.com/here/upload.php', { method: 'POST', body: formData })
+        .then(res => res.text())
+        .then(url => {
+            console.log('url:', url)
+            onSuccess(url)
+        })
+}
